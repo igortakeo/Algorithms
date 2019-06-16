f513c1610e7d02e2cc808cec7f68f2dee2eceec0
igortakeo <igor_takeo77@hotmail.com>
2019-06-16 19:33:17 -0300

    Update from my computer

--- dfs.cpp
+++ dfs.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 #define N 100100
 using namespace std;
+
 vector<int>vertices[N]; //lista de adjacências.
 int vis[N] = {0}; //0 para não visitado e 1 para visitado.
 
-void DFS(int x){ //x -> vértice inicial.
+void dfs(int x){ //x -> vértice inicial.
 	vis[x] = 1;
 	for(int i = 0; i < vertices[x].size(); i++){ //Vê se os vértices que estão ligados a x foram visitados. 
 		int v = vertices[x][i]; 
