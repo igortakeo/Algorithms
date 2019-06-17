95730cfb6589dde6a3fa5b3c28647495c501e2c3
igortakeo <igor_takeo77@hotmail.com>
2019-06-17 12:01:29 -0300

    Update from my computer

--- toposort.cpp
+++ toposort.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 vector<int>edges[MAX];//grafo
-stack<int> topo; //na stack topo vai conter a ordenacao topologico ao contrario, sendo a base da pilha o primeiro elemento
+stack<int> topo; //na stack topo vai conter a ordenacao topologico, sendo o topo da pilha o primeiro elemento
 bool vis[MAX];//vetor de visitados
 int number_vertex; //numero de vertices
 
@@ -12,7 +12,6 @@ void dfs_topo(int u){
 	for(auto v : edges[u]) if(!vis[v]) dfs_topo(v);	
 	topo.push(u); //coloco na pilha quando nao existe adjacente ou todos foram visitados	
 }
-
 
 void topological_sorting(){
 	for(int i=1; i<= number_vertex; i++) if(!vis[i]) dfs_topo(i); //ordenacao topologica vai passar por todos os vertices do grafo
