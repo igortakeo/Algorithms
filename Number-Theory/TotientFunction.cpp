f314f3af452c96392fc647de4ce1920639a9111b
igortakeo <igor_takeo77@hotmail.com>
2020-05-07 17:06:19 -0300

    uploaded TotientFunction.cpp

---
+++ Number-Theory/TotientFunction.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Complexity : O(sqrt(n))
+
+//Euler's Totient Function count the number of co-primes less than or equal to n
+//The sum of all values of Totient Function of all divisors of N is equal to N
+//For 2 numbers A and B if GCD(A,B) = 1 then phi(A)*phi(B) = phi(A*b) 
+
+double phi(int n){
+	double totient = n;
+	
+	for(int i=2; i*i<=n; i++){
+		if(n%i == 0){
+			while(n%i == 0) n/=i;
+			totient *= (1.0 -(1.0/(double)i));
+		}
+	}
+	if(n>1) totient *= (1.0 -(1.0/(double)n));
+	return totient;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	cout << phi(n) << endl;
+	
+	return 0;
+}
