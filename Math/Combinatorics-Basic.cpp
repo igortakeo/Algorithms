875f3ae80b83ffdef379af5b1edbd0ce8f28311a
igortakeo <igor_takeo77@hotmail.com>
2020-12-24 16:22:11 -0300

    Uploaded Combinatorics-Basic.cpp

---
+++ Math/Combinatorics-Basic.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MAX 15
+using namespace std;
+/*
+Simple Permutation: n!
+Arrangement: n!/(n-p)!
+Arrangement with Repetition: n^p
+Combination: n!/(p!*(n-p)!)
+Combination with Repetition: (n+p-1)!/(n-1)!*p!
+*/
+
+//Binomial Coefficient O(p) time
+int BinomialCoefficient(int n, int p){
+
+    int bc = 1;
+
+    //Using symmetry rule for binomial coefficient (C(n,q) = C(n,p) if q+p = n)
+    if(p > n-p) p = n - p;
+
+    //Calculating the binomial coefficient from simplify equation (n*(n-1)*...*(n-k+1))/(k*(k-1)*...1)
+    for(int i = 0; i < p; i++){
+        bc = (bc*(n-i))/(i+1);
+    }
+    return bc;
+}
+
+//Construct Pascal Triangle in O(n^2) time 
+int triangle[MAX][MAX];
+
+void PascalTriangle(int n){
+
+    for(int i = 0; i < n; i++){
+        for(int j=0; j<=i; j++){
+            if(i == j || j == 0) triangle[i][j] = 1;
+            //Using the recurrence for the binomial coefficients (n p) = (n-1 p-1) + (n-1 p)
+            else triangle[i][j] = triangle[i-1][j-1]+triangle[i-1][j]; 
+        }
+
+    }
+
+}
+
+
+/*
+    Inclusion and Exclusion
+*/
+
+// Problem example: how many numbers between [1,n] are divisible by numbers in vector 'numbers' ?
+// Complexity: O((2^n)*n)
+
+ll CardinalityofSum(ll n, vector<ll>numbers){
+
+    ll m = numbers.size();
+    ll subsets = 1<<m;
+    ll ans = 0;
+
+    //This first 'for' generate all combinations of subsets 
+    for(ll i=1; i < subsets; i++){
+        ll d = 1; 
+        ll number_setbits = __builtin_popcount(i); //Numbers of 1's in binary number i
+
+        //Checking if 'j' number in 'numbers' belongs in subset 'i'
+        for(ll j = 0; j<m; j++){
+            if(i & (1<<j)){
+                d *= numbers[j];
+            }
+        }
+
+        //If number of set bits in 'i' is odd or even
+        if(number_setbits & 1) ans += n/d;
+        else ans -= n/d;
+    }
+
+    return ans;
+}
