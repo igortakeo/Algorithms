502feb58b35e01ded822ed93ee63326df97247b6
igortakeo <igor.takeo@usp.br>
2021-10-02 14:52:11 -0300

    Test personal token

--- Data-Structures/DSU.cpp
+++ Data-Structures/DSU.cpp
@@ -13,10 +13,14 @@ void join(int x, int y){
 	
 	x = find(x); //pegando o pai de x.
 	y = find(y); //pegando o pai de y.		
+	
 	if(x==y) return; // se os dois tiverem o mesmo patriarca, quer dizer que ja estao unidos.
+
 	//se um dos dois patriarcas tiver peso menor, faca o menor ser filho do maior.
 	if(peso[x]<peso[y]){pai[x]=y; qtd[y]+=qtd[x];} 
+
 	else if(peso[x]>peso[y]){pai[y]=x; qtd[x]+=qtd[y];}
+	
 	//se os dois tiverem  o mesmo peso, escolha um para ser pai de outro e aumente seu peso.
 	else if(peso[x]==peso[y]){
 		pai[x]=y;
